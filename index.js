5999faba427eb3bdfe1be7409949370119094056
raju120405 <raju120405@gmail.com>
2022-05-25 23:13:30 +0600

    set token to local storage

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
@@ -18,12 +19,28 @@ try{
     await client.connect();
     console.log('database connected');
     const serviceCollection = client.db('assignment-12').collection('services');
+    const userCollection = client.db('assignment-12').collection('users');
+
     app.get('/service', async(req, res)=>{
         const query = {};
         const cursor = serviceCollection.find(query)
         const services = await cursor.toArray();
         res.send(services);
+    });
+
+    app.put('/user/:email',async(req,res)=>{
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email : email}
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
     })
+
 }
 finally{
 
